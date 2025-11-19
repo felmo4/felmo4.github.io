9797cd50f9d121aca661b66af3ca80201cca1195
Ralph Emmanuel Maglalang <121073436+felmo4@users.noreply.github.com>
2025-11-19 16:21:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // JavaScript function to switch tabs
 function showTab(tabId, dotId) {
 	// Hide all tab contents
@@ -34,32 +35,17 @@ function showTab(tabId, dotId) {
 
 }
 
+
+//Radio
+document.getElementById("radio").addEventListener("click", function () {
+	this.classList.add('scroll');
+	const audio = document.getElementById("myAudio");
+    audio.play();
+});
+
+
 // Slideshow
-let filteristIndex = 1, ohomeIndex = 1, laundryIndex = 1;
-
-showFilterist(filteristIndex);
-function plusFilterist(n) {
-showFilterist(filteristIndex += n);
-}
-function currentFilterist(n) {
-showFilterist(filteristIndex = n);
-}
-
-function showFilterist(n) {
-let i;
-let slides = document.getElementsByClassName("mySlidesFilterist");
-let dots = document.getElementsByClassName("dotFilterist");
-if (n > slides.length) {filteristIndex = 1}    
-if (n < 1) {filteristIndex = slides.length}
-for (i = 0; i < slides.length; i++) {
-	slides[i].style.display = "none";  
-}
-for (i = 0; i < dots.length; i++) {
-	dots[i].className = dots[i].className.replace(" active", "");
-}
-slides[filteristIndex-1].style.display = "block";  
-dots[filteristIndex-1].className += " active";
-}
+let ohomeIndex = 1
 
 showOhome(ohomeIndex);
 function plusOhome(n) {
@@ -85,43 +71,15 @@ slides[ohomeIndex-1].style.display = "block";
 dots[ohomeIndex-1].className += " active";
 }
 
-showLaundry(laundryIndex);
-function plusLaundry(n) {
-showLaundry(laundryIndex += n);
-}
-function currentLaundry(n) {
-showLaundry(laundryIndex = n);
-}
-
-function showLaundry(n) {
-let i;
-let slides = document.getElementsByClassName("mySlidesLaundry");
-let dots = document.getElementsByClassName("dotLaundry");
-if (n > slides.length) {laundryIndex = 1}    
-if (n < 1) {laundryIndex = slides.length}
-for (i = 0; i < slides.length; i++) {
-	slides[i].style.display = "none";  
-}
-for (i = 0; i < dots.length; i++) {
-	dots[i].className = dots[i].className.replace(" active", "");
-}
-slides[laundryIndex-1].style.display = "block";  
-dots[laundryIndex-1].className += " active";
-}
-
 
 // Scroll opacity effect
 document.getElementById("tab3").addEventListener("scroll", function () {
-	 let scroll2 = document.getElementById("scroll2");
-	 let scroll3 = document.getElementById("scroll3");
-	 let scroll4 = document.getElementById("scroll4");
-	 let scrollLeft = document.getElementById("tab3").scrollLeft;
-	 let scrollTop = document.getElementById("tab3").scrollTop;
+	 let scroll2 = document.getElementById("am-scroll2");
+	 let scroll3 = document.getElementById("am-scroll3");
+	 let scrollLeft = this.scrollLeft;
+	 let scrollTop = this.scrollTop;
 	 if (scrollLeft){
-		if (scrollLeft >= 1600) 
-			scroll4.classList.add('scroll');
-		else if (scrollLeft >= 1100) {
-			scroll4.classList.remove('scroll');
+		if (scrollLeft >= 1000) {
 			scroll3.classList.add('scroll');
 		}
 		else if (scrollLeft >= 500) {
@@ -133,13 +91,59 @@ document.getElementById("tab3").addEventListener("scroll", function () {
 	}
 	else{
 		if (scrollTop){
-		   if (scrollTop >= 900) 
-			   scroll4.classList.add('scroll');
-		   else if (scrollTop >= 700) {
-			   scroll4.classList.remove('scroll');
+		   if (scrollTop >= 900) {
 			   scroll3.classList.add('scroll');
 		   }
-		   else if (scrollTop >= 300) {
+		   else if (scrollTop >= 500) {
+			   scroll2.classList.add('scroll');
+			   scroll3.classList.remove('scroll');
+		   }
+		   else
+			   scroll2.classList.remove('scroll');
+	 }
+	}
+  });
+
+document.getElementById("tab2").addEventListener("scroll", function () {
+	 let scroll2 = document.getElementById("p-scroll2");
+	 let scroll3 = document.getElementById("p-scroll3");
+	 let scroll4 = document.getElementById("p-scroll4");
+	 let scroll5 = document.getElementById("p-scroll5");
+	 let scrollLeft = this.scrollLeft;
+	 let scrollTop = this.scrollTop;
+	 if (scrollLeft){
+		if (scrollLeft >= 3500) {
+			scroll5.classList.add('scroll');
+		}
+		else if (scrollLeft >= 2500) {
+			scroll4.classList.add('scroll');
+			scroll5.classList.remove('scroll');
+		}
+		else if (scrollLeft >= 1500) {
+			scroll3.classList.add('scroll');
+			scroll4.classList.remove('scroll');
+		}
+		else if (scrollLeft >= 500) {
+			scroll2.classList.add('scroll');
+			scroll3.classList.remove('scroll');
+		}
+		else
+			scroll2.classList.remove('scroll');
+	}
+	else{
+		if (scrollTop){
+		   if (scrollTop >= 1800) {
+			   scroll5.classList.add('scroll');
+		   }
+		   else if (scrollTop >= 1100) {
+			   scroll4.classList.add('scroll');
+			   scroll5.classList.remove('scroll');
+		   }
+		   else if (scrollTop >= 600) {
+			   scroll3.classList.add('scroll');
+			   scroll4.classList.remove('scroll');
+		   }
+		   else if (scrollTop >= 50) {
 			   scroll2.classList.add('scroll');
 			   scroll3.classList.remove('scroll');
 		   }
